7577b991be87e8a092c4dd05afe73fe6f5bc5918
Adam Wojciak <Code.AdamW78@gmail.com>
2025-02-02 14:19:28 -0500

    Fixed FXML loading

--- src/main/java/org/awdevelopment/smithlab/gui/FXMLResourceLoader.java
+++ src/main/java/org/awdevelopment/smithlab/gui/FXMLResourceLoader.java
@@ -1,5 +1,6 @@
 package org.awdevelopment.smithlab.gui;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.awdevelopment.smithlab.gui.controllers.AbstractController;
 import javafx.fxml.FXMLLoader;
@@ -9,32 +10,26 @@ import java.net.URL;
 
 public class FXMLResourceLoader {
 
+    private static final Level LOAD_MESSAGE_LOG_LEVEL = Level.DEBUG;
+
     public static FXMLResource load(FXMLResourceType fxmlResource, Logger logger) {
         Scene scene;
         AbstractController controller;
         try {
-            FXMLLoader loader = new FXMLLoader(getFxmlResourcePath(fxmlResource));
+            FXMLLoader loader = new FXMLLoader(getFxmlResourcePath(fxmlResource, logger));
             scene = new Scene(loader.load());
             controller = loader.getController();
         } catch (IOException e) {
-            String logMessage = "Failed to load FXML resource: " + fxmlResource;
-            logger.atFatal().log(logMessage);
-            logger.atFatal().log(e);
+            loggerHelper(logger, "Failed to load FXML resource: \"" + fxmlResource+"\"", e);
             throw new FailedToLoadFXMLException(fxmlResource);
         }
         return new FXMLResource(scene, controller);
     }
 
-    private static URL getFxmlResourcePath(FXMLResourceType fxmlResource) {
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        System.out.println("FXMLResourceLoader.class.getResource(\"/fxml/application.fxml\"): " + FXMLResourceLoader.class.getResource("/fxml/application.fxml"));
-        // System.out.println("getClass().getResource(\"/fxml/application.fxml\"): " + getClass().getResource("/fxml/application.fxml"));
-        System.out.println("classloader.getResource(\"/fxml/application.fxml\"): " + classloader.getResource("/fxml/application.fxml"));
-        return switch (fxmlResource) {
-            case APPLICATION -> FXMLResourceLoader.class.getResource("/fxml/application.fxml");
-            case CONDITIONS -> classloader.getResource("conditions.fxml");
-            case STRAINS -> classloader.getResource("strains.fxml");
-            case TIMEPOINTS -> classloader.getResource("timepoints.fxml");
-        };
+    private static URL getFxmlResourcePath(FXMLResourceType fxmlResource, Logger logger) {
+        loggerHelper(logger, "Loading FXML resource: \"" + fxmlResource + "\"");
+        return FXMLResourceLoader.class.getResource(fxmlResource.toString());
     }
+    private static void loggerHelper(Logger logger, String logMessage, Exception e) { logger.atFatal().log(logMessage, e);}
+    private static void loggerHelper(Logger logger, String logMessage) { logger.atLevel(FXMLResourceLoader.LOAD_MESSAGE_LOG_LEVEL).log(logMessage); }
 }
--- src/main/java/org/awdevelopment/smithlab/gui/FXMLResourceType.java
+++ src/main/java/org/awdevelopment/smithlab/gui/FXMLResourceType.java
@@ -1,5 +1,17 @@
 package org.awdevelopment.smithlab.gui;
 
 public enum FXMLResourceType {
-    APPLICATION, CONDITIONS, STRAINS, TIMEPOINTS
+    APPLICATION, CONDITIONS, STRAINS, TIMEPOINTS;
+
+    @Override
+    public String toString() { return getPath(this); }
+
+    private static String getPath(FXMLResourceType fxmlResource) {
+        return switch (fxmlResource) {
+            case APPLICATION -> "/fxml/application.fxml";
+            case CONDITIONS -> "/fxml/conditions.fxml";
+            case STRAINS -> "/fxml/strains.fxml";
+            case TIMEPOINTS -> "/fxml/timepoints.fxml";
+        };
+    }
 }
